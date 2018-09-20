66d5e286bf41641aa0a3ad841520ae03d57e8a33
laraismael1 <laraismael123@gmail.com>
2018-09-20 12:51:45 -0700

    basic server set up

--- server/src/lib/server.js
+++ server/src/lib/server.js
@@ -3,10 +3,10 @@
 import cors from 'cors';
 import express from 'express';
 import mongoose from 'mongoose';
-import bodyParser from 'bodyParser';
+import bodyParser from 'body-parser';
 
 const app = express();
-const router = epress.Router();
+const router = express.Router();
 
 // env variables
 const PORT = process.env.PORT || 3000;
@@ -15,7 +15,7 @@ const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mern-starter
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
-app.use(boduParser.json(), cors())
+app.use(bodyParser.json(),cors())
 
 app.use(require('../route/auth-router'));
 
@@ -27,14 +27,15 @@ app.all('*', (request, response) => {
 // error middleware
 app.use(require('./error-middleware'));
 
+
 export const start = () => {
-  app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+  app.listen(PORT, () =>{
+    console.log(`Listening on port: ${PORT}`)
   })
 }
 
 export const stop = () => {
   app.close(PORT, () => {
-    console.log(`Shut down on port ${PORT}`);
+    console.log(`Shut down on port: ${PORT}`)
   })
 }
